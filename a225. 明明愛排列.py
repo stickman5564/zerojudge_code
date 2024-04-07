3d525457b05a834921aa68a287324ca3585a557c
stickman5564 <106570181+stickman5564@users.noreply.github.com>
2024-04-07 22:52:11 +0800

    Add files via upload

--- a225. 明明愛排列.py
+++ a225. 明明愛排列.py
@@ -1,1 +1,11 @@
-
+# completed
+while 1:
+    try:
+        input()
+        list_0 = list(map(int,input().split()))
+        list_0.sort(key=lambda x: (x % 10, -x // 10))
+        list_0 = [str(i) for i in list_0]
+        ans = " ".join(list_0)
+        print(ans)
+    except EOFError:
+        break
