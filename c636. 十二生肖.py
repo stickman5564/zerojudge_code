2b8eb5cb039f8bda4acdd0aff208f4680c7705b1
stickman5564 <106570181+stickman5564@users.noreply.github.com>
2024-04-07 19:20:16 +0800

    Update c636. 十二生肖.py

--- c636. 十二生肖.py
+++ c636. 十二生肖.py
@@ -1,6 +1,12 @@
 # completed
 from sys import stdin
 list_0 = ["鼠","牛","虎","兔","龍","蛇","馬","羊","猴","雞","狗","豬"]
+list_1 = list_0[::-1]
 for line in stdin:
-    n = int(line) - 1
-    print(list_0[n%12])
+    n = int(line)
+    if n >=0:
+        n = (n - 1) % 12
+        print(list_0[n])
+    else:
+        n = (abs(n) - 1) % 12
+        print(list_1[n])
