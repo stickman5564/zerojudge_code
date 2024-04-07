c93429d0c62920ccc85b615e2677a4eb7d9591e2
stickman5564 <106570181+stickman5564@users.noreply.github.com>
2024-04-07 15:19:08 +0800

    Update and rename c726.K-I-S-S-I-N-G.py to c726. K-I-S-S-I-N-G (一行版).py

---
+++ c726. K-I-S-S-I-N-G (一行版).py
@@ -0,0 +1,3 @@
+# completed
+name1, name2 =  input().split()
+print(name1,"and",name2,"sitting in the tree")
--- c726.K-I-S-S-I-N-G.py
+++
@@ -1,3 +0,0 @@
-# completed
-name1, name2 =  input().split()
-print(name1,"and",name2,"sitting in the tree")
